1534c3e87cf2a5d85679aa948450681a2d51e2d2
jokermon <85845434+jokermon@users.noreply.github.com>
2021-07-30 20:48:32 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -119,13 +119,14 @@ async def auto_filter(bot, update):
                 bot_ = FIND.get("bot_details")
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
-            results.append(
-                [
-                    InlineKeyboardButton(button_text, url=file_link)
-                ]
-            )
-        
-    else:
+            results.append([
+            InlineKeyboardButton("📂 " + file_names, url=file_link),
+            InlineKeyboardButton(f_size, url=file_link)
+        ])
+                
+                    
+                
+     else:
         return # return if no files found for that query
     
 
